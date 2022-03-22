4b405453d8df0e90edf0fb05b9cceb941073dff8
usmanprasetyo <65372288+usmanprasetyo@users.noreply.github.com>
2022-03-22 08:28:31 +0700

    bff-2.py

--- bff-2.py
+++ bff-2.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("crackd").masuk()
+       __import__("cracks").masuk()
    except Exception as e:
        exit(str(e))
